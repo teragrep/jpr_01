37b140ba845104a989bbca7aef3416dab72e84ef
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2024-07-22 16:20:31 +0300

    Remove Main.java, commented out imports

--- src/test/java/com/teragrep/jpr_01/JavaPcreIT.java
+++ src/test/java/com/teragrep/jpr_01/JavaPcreIT.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// import static org.junit.jupiter.api.Assertions.*;
 
 class JavaPcreIT {
     private static final Logger LOGGER = LoggerFactory.getLogger(JavaPcreIT.class);
